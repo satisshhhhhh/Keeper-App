3f79ca3bb7a66ab044463e0d2cd493be2c026e1d
Satish Panchal <satishpanchal651@gmail.com>
2021-10-07 21:48:46 +0530

    first-commit

---
+++ build/precache-manifest.3dc96e80f363ac025aea3d9c7bdc85bb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1794084e53e639e2cd820d4db6d1d1",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "4b184f0b9c47ebc07f24",
+    "url": "/keeper-app/static/js/2.865dfa49.chunk.js"
+  },
+  {
+    "revision": "3bb550695abc4b624ded",
+    "url": "/keeper-app/static/js/main.302b876a.chunk.js"
+  },
+  {
+    "revision": "fb7d45a05bf0dfe0fe92",
+    "url": "/keeper-app/static/js/runtime-main.42e7a143.js"
+  }
+]);
