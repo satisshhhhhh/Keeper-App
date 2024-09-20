20708e06a2d72bc38af0ac802446b1d6d1781f59
Satish Panchal <58567211+satisshhhhhh@users.noreply.github.com>
2024-09-21 00:18:32 +0530

    Update Note.jsx
    Fixed the Delete button

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -9,7 +9,10 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <button style={{
+        width: 33%;
+        border-radius: 0.5rem;
+      }} onClick={handleClick}>DELETE</button>
     </div>
   );
 }
